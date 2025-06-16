c755beaac03e0b2dacb0b1ff1224227d1c1697dc
Caleb N <calebn8010@gmail.com>
2025-06-16 08:35:28 -0400

    new Search component

--- ReactApp1.client/src/Pages/Home.tsx
+++ ReactApp1.client/src/Pages/Home.tsx
@@ -1,11 +1,16 @@
 import WeatherForecast from "../Components/WeatherForecast.tsx";
 import LogoutLink from "../Components/LogoutLink.tsx";
 import AuthorizeView, { AuthorizedUser } from "../Components/AuthorizeView.tsx";
+import SearchForm from "../Components/DealSearch.tsx";
 
 function Home() {
     return (
         <AuthorizeView>
             <span><LogoutLink>Logout <AuthorizedUser value="email" /></LogoutLink></span>
+            <div className="min-h-screen flex items-center justify-center bg-gray-100">
+                <p> Search for a PC game to show possible matches to add into your wishlist</p>
+                <SearchForm />
+            </div>
             <WeatherForecast />
         </AuthorizeView>
     );
