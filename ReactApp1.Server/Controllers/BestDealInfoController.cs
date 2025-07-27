e788677444fb8c5b174454e298ff02cb54660be3
Caleb <Caleb@DESKTOP-OVKUJ4E>
2025-07-27 15:14:38 -0400

    Working request

---
+++ ReactApp1.Server/Controllers/BestDealInfoController.cs
@@ -0,0 +1,31 @@
+﻿using EasyDeal.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using ReactApp1.Server.Models;
+
+
+namespace EasyDeal.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BestDealInfoController : ControllerBase
+    {
+        private readonly ILogger<BestDealInfoController> _logger;
+
+        public BestDealInfoController(ILogger<BestDealInfoController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Handles Post request from front end for BestDealInfo endpoint
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] BestGameDealRequest request)
+        {
+            Console.WriteLine("BestDealInfoController Post method called.");
+            
+            // Fetch the list of game deals based on the query
+            var dealInfo = await CheapSharkApiRequests.GameInfoById(request.gameID);
+            
+            return Ok(dealInfo); // Serialize detailed deal info to JSON
+        }
+    }
+}
