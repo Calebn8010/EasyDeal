b5357ce2b3e35100f28f84b5053a499048baa3b2
Caleb N <calebn8010@gmail.com>
2025-08-10 12:47:32 -0400

    Test new endpoint controller

---
+++ ReactApp1.Server/Controllers/WishlistUpdatesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ReactApp1.Server.Models;
+
+namespace EasyDeal.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WishlistUpdatesController : ControllerBase
+    {
+        private readonly ILogger<WishlistUpdatesController> _logger;
+
+        public WishlistUpdatesController(ILogger<WishlistUpdatesController> logger)
+        {
+            _logger = logger;
+        }
+
+
+
+        // Handles Post request from front end for WishlistUpdates endpoint
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] GameDeal request)
+        {
+            _logger.LogInformation("WishlistUpdatesController Post method called.");
+            AddToWishlist(request);
+
+            return Ok(new { message = "Wishlist updated successfully." });
+        }
+
+        public bool AddToWishlist(GameDeal gameDeal)
+        {
+            _logger.LogInformation($"Game deal to add: {gameDeal}");
+            // Implement your logic to add the game deal to the wishlist
+            // For example, you might save it to a database or an in-memory list
+            // Return true if the operation was successful, otherwise false
+            return true;
+        }
+    }
+}
